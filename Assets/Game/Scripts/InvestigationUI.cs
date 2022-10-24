55b05ee7538200b12ef6166aed1e9cd31e3122e8
NagayaMiyu <71922638+wonkle07@users.noreply.github.com>
2022-10-24 14:19:01 +0900

    スクリプト内のコメント追加

--- Assets/Game/Scripts/InvestigationUI.cs
+++ Assets/Game/Scripts/InvestigationUI.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine.InputSystem;
+
+
 public class InvestigationUI : MonoBehaviour
 {
+    [SerializeField]
+    [Header("表示されているか")]
+    public bool dislay;
 
     private void Start()
     {
@@ -22,13 +27,13 @@ public class InvestigationUI : MonoBehaviour
     {
         this.gameObject.SetActive(false);
 
-        //調査の範囲に入るまで待機
+        //調査の範囲に入るまで待機（今は左クリックで代用）
         await UniTask.WaitUntil(() => Mouse.current.leftButton.wasPressedThisFrame, cancellationToken: token);
 
         //以下UI表示
         this.gameObject.SetActive(true);
 
-        //範囲外に出たら消す
+        //範囲外に出たら消す（今はスペースキーで代用）
         await UniTask.WaitUntil(() => Keyboard.current.spaceKey.wasPressedThisFrame, cancellationToken: token);
 
         this.gameObject.SetActive(false);
