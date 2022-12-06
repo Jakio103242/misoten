c974ced6cc982883e62340b615688279ef9ced0d
NagayaMiyu <71922638+wonkle07@users.noreply.github.com>
2022-12-06 15:42:10 +0900

    チームロゴの表示実装

--- Assets/Game/Scripts/TeamLogoScene.cs
+++ Assets/Game/Scripts/TeamLogoScene.cs
@@ -5,45 +5,13 @@ using UnityEngine.UI;
 
 public class TeamLogoScene : MonoBehaviour
 {
-    [SerializeField]
-    private Image fadeImage;
-
-    private bool isFadeIn = true;
-    private float alpha = 0.0f;
-
     void Start()
     {
-        isFadeIn = true;
-        StartCoroutine("MovetoTitleScene");
     }
 
     void Update()
     {
+
     }
 
-    IEnumerator MovetoTitleScene()
-    {
-        if (isFadeIn == true)
-        {
-            alpha += Time.deltaTime / 5.0f;
-
-            //フェードアウト終了判定
-            if (alpha >= 255.0f)
-            {
-                isFadeIn = false;
-                alpha = 255.0f;
-                Debug.Log("aa");
-            }
-
-            //フェード用Imageの色・透明度設定
-            fadeImage.color = new Color(255.0f, 255.0f, 255.0f, alpha);
-        }
-        else
-        {
-            //1秒待つ
-            yield return new WaitForSeconds(1);
-
-            //FadeManager.FadeOut("TitleScene", 0.5f);
-        }
-    }
 }
