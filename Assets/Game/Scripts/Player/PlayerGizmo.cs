8908b7c04ce8329c2a5d2ff1a2f4e895127f4a58
HAL-HatanakaYu <82099820 +HAL-HatanakaYu@users.noreply.github.com>
2022-12-17 09:26:46 +0900

    player関連のスクリプ更新

--- Assets/Game/Scripts/Player/PlayerGizmo.cs
+++ Assets/Game/Scripts/Player/PlayerGizmo.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UniRx;
 
 namespace Game.Player
--- Assets/Game/Scripts/Player/PlayerInvestigate.cs
+++ Assets/Game/Scripts/Player/PlayerInvestigate.cs
@@ -1,21 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Game.Input;
+using Game.Intaract;
+using Game.Story;
 
 public class PlayerInvestigate : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    Vector3 viewportPos;
-    float displayDistance;
-    float investigateDistance;
-    
+    [SerializeField] InputReader input;
+    [SerializeField] DialogueManager dialogueManager;
+    [SerializeField] ObjectManager objectManager;
+
     void Start()
     {
-
+        // input
     }
 
     void Update()
     {
-        
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Intaractable investigatable = objectManager.NearestIntaractableObject();
+            if(investigatable != null)
+            {
+                //dialogueManager.SetDialogue(((StoryInvestigationEvent)investigatableObject.StoryIncident()).DialogueData);
+                investigatable.Completed = true;
+            }
+        }
     }
 }
